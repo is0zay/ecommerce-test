7884f767c22d1d91d80aa11bb5d124c0ba0852b3
Isaiah <isozay0@gmail.com>
2023-05-06 00:50:40 -0400

    started responsive media queries

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
 const carousel = document.querySelector(".section3-carousel");
-firstSlide = carousel.querySelectorAll("img")[0];
-arrowButtons = document.querySelectorAll(".wrapper i");
+const firstSlide = carousel.querySelectorAll("img")[0];
+const arrowButtons = document.querySelectorAll(".wrapper i");
 
 let dragStartPostition = false, prevPageX, prevScrollLeft;
-let firstSlideWidth = firstSlide.clientWidth + 4; // first slide width & adding 4px margin value
+let firstSlideWidth = firstSlide.clientWidth + 4;
+// first slide width & adding 4px margin value
 
-arrowButtons.forEach(icon => {
+arrowButtons.forEach((icon) => {
     icon.addEventListener("click", () => {
-        carousel.scrollLeft += icon.id == "left-arrow" ? -firstSlideWidth : firstSlideWidth; // if left icon is clicked, reduce the width value from the carousel scroll left, else add to it
+        carousel.scrollLeft += icon.id == "left-arrow" ? -firstSlideWidth : firstSlideWidth;
+        // if left icon is clicked, reduce the width value from the carousel scroll left, else add to it
     });
 });
 
-const dragBegin = (e) => {
+const dragBegin = function(e) {
     // updating global variables value on mouse down event
-    dragStartPostition = true;   // By default isDragging is fals and it will only be true if the mouse btn is clicked
+    dragStartPostition = true;
+    // By default isDragging is false
+    // and it will only be true if the mouse btn is clicked
     prevPageX = e.pageX;
-    prevScrollLeft = carousel.scrollLeft; // scrollLeft provides the number of pixels of the element content that is scrolled horizontally
-}
+    prevScrollLeft = carousel.scrollLeft;
+    // scrollLeft provides the number of pixels of the
+    // element content that is scrolled horizontally
+};
 
-const dragging = (e) => {
+const dragging = function(e) {
     // scrolling to the left according to mouse pointer
-    if(!dragStartPostition) return;
-    e.preventDefault(); //This prevents images from dragging initially
+    if(!dragStartPostition) {return;}
+    e.preventDefault();
+    //This prevents images from dragging initially
     // carousel.classList.add("dragging");
     let positionDifference = e.pageX - prevScrollLeft;
     carousel.scrollLeft = prevScrollLeft - positionDifference;     //scrollLeft set or return the number of pixels an element's content is crolled horizontally
